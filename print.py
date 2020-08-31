26500056d0070a872c42272698400c16fc55557a
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-08-31 13:45:55 +0200

    print övning

---
+++ print.py
@@ -0,0 +1,3 @@
+print("Jag heter Hugo och är uppväxt i Onsala.")
+print("Jag har testat att programera i Java tidigare men inget avancerat.")
+print("Jag har sökt för att detta är något som jag vill jobba med i framtiden och jag har alltid tyckt programmering varit intressant.")
