8bc276a18054bb552136cd7bc2347ce668e1534e
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-01 11:24:30 +0200

    fixed mainloop

--- number_guess.py
+++ number_guess.py
@@ -1,24 +1,25 @@
 import random
 
-max_guesses = 3
+max_guesses = 0
 
 n = random.randint(1, 100)
 print("Jag tänker på ett tal mellan 1 och 100, vilket tal tänker jag på?")
 
-for guesses in range(max_guesses):
-    text = input("Din gissar: ")
-    as_number = int(text)
 
-    if as_number == n:
-        print("Korrekt! Det tog dig")
-        break
+def mainloop(max_guesses, n):
+    while True:
+        text = input("Din gissar: ")
+        as_number = int(text)
 
-    if as_number < n:
+        if as_number == n:
+            print("Korrekt! Det tog dig" + str(max_guesses) + " gissningar")
+            break
+
+        if as_number < n:
             print("Fel nummer, mitt nummer är högre... Försök igen! ")
 
-    if as_number > n:
+        if as_number > n:
             print("Fel nummer, mitt nummer är lägre... Försök igen! ")
 
-else:
-    raise ValueError("Detta var inte din grej Alla dina " + max_guesses + "är slut.")
 
+mainloop(max_guesses, n)
