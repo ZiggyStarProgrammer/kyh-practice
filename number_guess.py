cecfa245b1f784d90d0654199bf001e82056c3a6
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-01 11:37:36 +0200

    fixed guess count

--- number_guess.py
+++ number_guess.py
@@ -10,16 +10,19 @@ def mainloop(max_guesses, n):
     while True:
         text = input("Din gissar: ")
         as_number = int(text)
+        max_guesses = max_guesses + 1
 
         if as_number == n:
-            print("Korrekt! Det tog dig" + str(max_guesses) + " gissningar")
+            print("Korrekt! Det tog dig " + str(max_guesses) + " gissningar")
             break
 
         if as_number < n:
             print("Fel nummer, mitt nummer är högre... Försök igen! ")
+            print("Du har " + str(max_guesses))
 
         if as_number > n:
             print("Fel nummer, mitt nummer är lägre... Försök igen! ")
+            print("Du har " + str(max_guesses))
 
 
 mainloop(max_guesses, n)
