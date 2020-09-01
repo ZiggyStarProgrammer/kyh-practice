5fb4f5ca09707bea3bbc15988c2e63e124cc89cc
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-01 14:12:33 +0200

    ask_number function fixed, program working properly

--- number_guess.py
+++ number_guess.py
@@ -1,34 +1,35 @@
 import random
 
 max_guesses = 0
-
-n = random.randint(1, 100)
+n = random.randint(1, 5)
 print("Jag tänker på ett tal mellan 1 och 100, vilket tal tänker jag på?")
 
 
-def mainloop(max_guesses, n):
+def ask_number(max_guesses):
+    max_guesses = max_guesses + 1
+    text = input("Vad är din gissning?")
+    as_number = int(text)
+    mainloop(max_guesses, n, as_number)
+    return as_number
+
+
+def mainloop(max_guesses, n, as_number):
     while True:
-        text = input("Din gissar: ")
-        as_number = int(text)
-        max_guesses = max_guesses + 1
 
         if as_number == n:
             print("Korrekt! Det tog dig " + str(max_guesses) + " gissningar")
-            break
+            return
 
         if as_number < n:
             print("Fel nummer, mitt nummer är högre... Försök igen! ")
-            print("Du har gjort " + str(max_guesses) + "gissningar")
+            print("Du har gjort " + str(max_guesses) + " gissningar")
+            ask_number(max_guesses)
 
         if as_number > n:
             print("Fel nummer, mitt nummer är lägre... Försök igen! ")
             print("Du har gjort " + str(max_guesses) + " gissningar")
+            ask_number(max_guesses)
+        break
 
 
-def ask_number():
-    text = input()
-    tal = int(text)
-    mainloop(max_guesses, n)
-
-
-ask_number()
+ask_number(max_guesses)
