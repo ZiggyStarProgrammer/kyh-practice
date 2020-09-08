5084014279a0503c3dc1b170e53cc699ad9eb854
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-08 15:01:09 +0200

    uppgift

---
+++ test path.py
@@ -0,0 +1,6 @@
+import pathlib
+import os
+
+os.getcwd()
+
+p = pathlib.Path()
---
+++ uppgift16.py
@@ -0,0 +1,27 @@
+import pathlib
+
+
+def case():
+    p = pathlib.Path('system.log')
+    content = p.read_text()
+
+    def wordcount(txt):
+        dokument = "system.log"
+        important = []
+        keep_phrases = ["BEAR", "X-RAY"]
+
+        with open(dokument) as f:
+            f = f.readlines()
+
+        for line in f:
+            line = line.strip()
+            for phrase in keep_phrases:
+                if phrase in line:
+                    important.append(line)
+                    break
+
+        print(*important, sep='\n')
+
+
+case()
+
