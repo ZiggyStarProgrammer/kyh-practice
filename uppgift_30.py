d67c027f832cee6e809f97a43a2bb7a526a104ec
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-21 17:23:55 +0200

    fixed odd and even numbers and cleaned upp prints

--- uppgift_30.py
+++ uppgift_30.py
@@ -5,7 +5,7 @@
 #       f"Siffror: {reg[3:]}")
 
 
-numbers = input("Ange ett tal med komma emellan: ")
+numbers = input("Ange ett tal med komma emellan: ").strip(",")
 tal = []
 for ele in numbers.split(","):
     tal.append(int(ele))
@@ -17,10 +17,14 @@ print(f"Summan av talen: {sum(tal2)} \n")
 # Variant 1 för att köra baklänges
 print(f"Talen baklänges: {tal2[::-1]}")
 # Variant 2
-backwards = "".join(list(reversed(numbers)))
+backwards = " ".join(list(reversed(numbers)))
 print(f"Talen baklänges: {backwards}")
 
+# printa varannan index
+print(f"Varannat udda index: {tal2[0:len(tal2):2]}")
+print(f"Varannat jämna index: {tal2[1:len(tal2):2]}")
 
-odd = [num for num in numbers.split(",") if int(num) % 2 != 0]
-print(f"Udda tal: {odd[0:len(odd)]}")
-# print(f"Jämna tal: {odd[1:len(odd):2]}")
+odd = [num for num in numbers.split(",") if int(num) % 2 == 1] # detta är ett exempel på incomprehension
+even = [num for num in numbers.split(",") if int(num) % 2 == 0]
+print(f"Udda tal:", ", ".join(odd[0:len(odd)]))
+print(f"Jämna tal:", ", ".join(even[0:len(even)]))
