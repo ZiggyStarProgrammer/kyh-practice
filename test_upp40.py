3625bae329b911ebd6d3df1cf764a15a63fe4cea
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-29 11:19:28 +0200

    another test

---
+++ test_upp40.py
@@ -0,0 +1,16 @@
+from uppgift_40 import reverse
+
+def test_rev():
+    expected = [3, 2, 1]
+    got = reverse([1, 2, 3])
+    assert expected == got
+
+def test_rev_char():
+    expected = ["c", "b", "a"]
+    got = reverse(["a", "b", "c"])
+    assert expected == got
+
+def test_rev_wrong_in():
+    expected = [3, 2, 1]
+    got = reverse([1, 3, 2])
+    assert expected == got
--- uppgift_40.py
+++ uppgift_40.py
@@ -1,9 +1,14 @@
-def reverse():
-    listo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def reverse(normal_list):
+    listo = normal_list
     otsil = []
     for i in range(len(listo) - 1, -1, -1):
         otsil.append(listo[i])
     return otsil
 
+def rev(normal):
+    rev = normal[::-1]
+    return rev
 
-print(reverse())
+
+print(reverse([1, 2, 3]))
+print(rev("1, 2, 3"))
