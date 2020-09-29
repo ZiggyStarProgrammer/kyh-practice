856badf072cf17c932676afeeae4d6f54199f330
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-09-29 11:31:37 +0200

    another test

--- test_upp40.py
+++ test_upp40.py
@@ -1,4 +1,4 @@
-from uppgift_40 import reverse
+from uppgift_40 import reverse, upper
 
 def test_rev():
     expected = [3, 2, 1]
@@ -13,4 +13,9 @@ def test_rev_char():
 def test_rev_wrong_in():
     expected = [3, 2, 1]
     got = reverse([1, 3, 2])
+    assert expected == got
+
+def test_upper():
+    expected = 2
+    got = upper("hej HopPop")
     assert expected == got
--- uppgift_40.py
+++ uppgift_40.py
@@ -5,10 +5,35 @@ def reverse(normal_list):
         otsil.append(listo[i])
     return otsil
 
+# annat sätt att skriva baklänges fast som en sträng
 def rev(normal):
     rev = normal[::-1]
     return rev
 
 
+def upper(text):
+    counter = 0
+    for c in text:
+        if c.isupper():
+            counter += 1
+    return counter
+
+
+def upperC(text):
+    chars = []
+    counter = 0
+    for c in text:
+        if c.isupper():
+            counter += 1
+            chars.append(c)
+    return chars
+
+
+def blank():
+    counter = 0
+
+
+
 print(reverse([1, 2, 3]))
 print(rev("1, 2, 3"))
+print(upper("hej HoPop"))
