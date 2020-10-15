82d9484a05b9361586134b0a8f379e421e41e3ca
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-10-15 11:11:13 +0200

    basic settings

---
+++ basics_exercises.py
@@ -0,0 +1,61 @@
+name = input("Name: ")
+print(f"Your name is: {name.title()}")
+
+num1 = int(input("Write number: "))
+num2 = int(input("Write a number to multiply: "))
+result = num1 * num2
+print(result)
+
+
+result1 = (3+2) * 6
+print(result1)
+
+annoying_bot = True
+while annoying_bot:
+    vowel = ["a", "o", "e", "i"]
+    check_this = input("Skriv text: ")
+    for i in check_this:
+        if i.lower() in vowel:
+            print(i)
+            annoying_bot = False
+
+age = int(input("How old are you?"))
+if age < 12:
+    print("Grow up kid")
+if 12 < age < 20:
+    print("You are a teen!")
+if 20 < age < 40:
+    print("Get your life together!")
+if age > 40:
+    print("u old...")
+
+money = {
+    "1": "George Washington",
+    "2": "Thomas Jefferson",
+    "5": "Abraham Lincoln",
+    "10": "Alexander Hamilton",
+    "20": "Andrew Jackson",
+    "50": "Ulysses S.Grant",
+    "100": "Benjamin Franklin"
+}
+note = input("Write the number of a bill: ")
+print(money[note])
+
+X = int(input("Tal: "))
+Y = int(input('Tal: '))
+Z = int(input('Tal: '))
+
+x = 0
+y = 0
+z = 0
+
+if X < Y and X < Z:
+    x = X
+
+if Y < X and Y < Z:
+    x = Y
+
+if Z < X and Z < Y:
+    x = Z
+
+print(x)
