e1ed14c8287b399b51dff4d01d732a09c276f5a0
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-11-12 11:34:22 +0100

    weird stuff is happenning now

---
+++ slicing.py
@@ -0,0 +1,22 @@
+ls = ["hej", "hopp", "hohoo", "tjohoj", "tjenixen"]
+
+# printar listan baklänges
+print(ls[::-1])
+
+# printar varannan
+print(ls[::2])
+
+# prinar index 0
+print(ls[0])
+
+# printar varannan och börjar på index 1 i listan
+print(ls[1::2])
+
+#printar backlänges från och med index 3
+print(ls[3::-1])
+
+#printar de sista två index
+print(ls[:2:-1])
+
+#printar varannan baklänges
+print(ls[::-2])
