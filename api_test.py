0d370c299d1c5625a32bc125e7c40732c5c424b3
ZiggyStarProgrammer <hugo.ojdal@gmail.com>
2020-11-19 09:25:36 +0100

    leker runt med API

--- api_test.py
+++ api_test.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def movie_req(name):
     r = requests.get("http://www.omdbapi.com/", params={"t": name, "apikey": "9f6d550c"})
     return r.json()
--- test_conversions.py
+++ test_conversions.py
@@ -1,5 +1,6 @@
 from conversions import m_to_mm, cm_to_m, mm_to_m
 
+
 def test_conversion_from_meters_to_mm():
     expected = 1000
     got = m_to_mm(m=1)
@@ -8,10 +9,12 @@ def test_conversion_from_meters_to_mm():
     got_2 = m_to_mm(m=5)
     assert expected_2 == got_2
 
+
 def test_cm_to_m():
     expected = 2
     got = cm_to_m(cm=200)
     assert expected == got
+
 
 def test_mm_to_m():
     expected = 1
